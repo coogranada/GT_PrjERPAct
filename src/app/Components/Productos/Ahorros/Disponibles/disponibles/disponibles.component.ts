9ee31353d7971f7d8f97f48544e78eec4bdbf286
ysalazarm <42752960+ysalazarm@users.noreply.github.com>
2025-06-17 08:18:36 -0500

    Correcciones disponible

--- src/app/Components/Productos/Ahorros/Disponibles/disponibles/disponibles.component.ts
+++ src/app/Components/Productos/Ahorros/Disponibles/disponibles/disponibles.component.ts
@@ -4218,7 +4218,7 @@ export class DisponiblesComponent implements OnInit {
               }
               // fin notificador  
               // liberar tarjeta o libreta
-              if (this.DisponibleForm.get('IdMedioPago')?.value !== 60 || this.DisponibleForm.get('IdMedioPago')?.value !== 70){
+              if (this.DisponibleForm.get('IdMedioPago')?.value !== 60 && this.DisponibleForm.get('IdMedioPago')?.value !== 70){
                 swal.fire({
                   title: '¿Desea liberar tarjeta y/o libreta de cuenta anulada?',
                   icon: 'question',
