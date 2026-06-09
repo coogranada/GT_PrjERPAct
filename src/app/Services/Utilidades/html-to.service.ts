ef1cc40ab26f1ef316bace17d0f0a03bcbdbc9d7
ysalazarm <42752960+ysalazarm@users.noreply.github.com>
2026-06-09 07:46:16 -0500

    ajuste de para mejorar lentitud

--- src/app/Services/Utilidades/html-to.service.ts
+++ src/app/Services/Utilidades/html-to.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import jsPDF from 'jspdf';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +6,38 @@ import jsPDF from 'jspdf';
 export class HtmlToService {
 
   constructor() { }
-  HtmlToPdf(html : string,orientation : "p" | "portrait" | "l" | "landscape", format : number[]){
-    const doc = new jsPDF({orientation : orientation,unit: 'px',format : format});
+
+  async HtmlToPdf(html: string, orientation: "p" | "portrait" | "l" | "landscape", format: number[]) {
+
+  try {
+
+    const { default: jsPDF } = await import('jspdf');
+
+    const doc = new jsPDF({
+      orientation: orientation,
+      unit: 'px',
+      format: format
+    });
 
     const content = document.getElementById(html);
+
     if (content) {
+
       doc.setFontSize(12);
+
       doc.html(content, {
-        callback: (doc : any) => {
+        callback: (doc: any) => {
           doc.save(html + '.pdf');
         }
       });
+
+    } else {
+      console.error("Elemento HTML no encontrado");
     }
+
+  } catch (error) {
+    console.error("Error generando PDF", error);
   }
+
+ }
 }
